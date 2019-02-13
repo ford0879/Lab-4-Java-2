fcbd1692f5c95a8de412c7037123c2a005c7d394
Derek Ford <derekford@ou.edu>
2019-02-12 20:27:53 -0600

    Created list of what needs to be done and how long it took for each task

---
+++ Todo.java
@@ -0,0 +1,36 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-11
+OU ID_______________: 113305683
+OU EMAIL____________: derekford@ou.edu
+
+Tasks:
+1. Animal class full constructor and getters test
+2. Animal class toString test
+3. Equipment class constructor and getters test
+4. Equipment class toString test 
+5. Zoo class constructor and getters test
+6. Zoo class expandZoo test
+7. Zoo class addAnimal test
+8. Zoo class toString test
+
+Estimated Time (minutes):
+1. 15 
+2. 10
+3. 15
+4. 10
+5. 15
+6. 20
+7. 15
+8. 10
+
+Actual Time (minutes):
+1. 15
+2. 10
+3. 30
+4. 35
+5. 35
+6. 15
+7. 15
+8. 25
+*/}
